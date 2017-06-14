0a35a1756eac9f2dc1551a0f3088b442c36d476d
Ivan Startsev <istartsev@hystax.com>
2017-06-14 15:04:24 +0300

    added settings.py file to gitignore

--- tests/settings.py
+++ tests/settings.py
@@ -1,9 +1,9 @@
 class S3Settings:
-    S3_LOGIN = 'your_login'
-    S3_PASSWORD = 'your_password'
-    S3_ACCESS_KEY = 'your_a_key'
-    S3_SECRET_KEY = 'your_s_key'
+    S3_LOGIN = 'sbsupport@securedata.com'
+    S3_PASSWORD = 'sbsupportamazon16'
+    S3_ACCESS_KEY = 'AKIAJZNFLQI5MJTHFENA'
+    S3_SECRET_KEY = '9zu26ktnVgQloquFhN5hNRPEDiw9IomZ9i/aK/po'
 
-    BUCKET_NAME = 'your_bucket_name'
+    BUCKET_NAME = 'SecureCLoud_bucket_st1'
     REGION = 'us-east-1'
     TEST_FOLDER = 'testdata'
